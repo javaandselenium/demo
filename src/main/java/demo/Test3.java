1744161538f8827247fe6f168e8441d30f2b7976
QSP <QSP@DESKTOP-EVK9GMC>
2021-01-13 15:50:29 +0530

    h

---
+++ src/main/java/demo/Test3.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import repackage.Repackage;
+
+public class Test3 {
+	@Test
+	public void demo() {
+		Reporter.log("hello",true);
+	}
+
+}
