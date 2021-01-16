6731c43a9e41098e553ee678c215feb805a5bff5
QSP <QSP@DESKTOP-EVK9GMC>
2021-01-16 09:54:10 +0530

    hiii

---
+++ src/main/java/demo/Test9.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import repackage.Repackage;
+
+public class Test9 {
+	@Test
+	public void demo() {
+		Reporter.log("hello guys ge",true);
+	}
+
+}
